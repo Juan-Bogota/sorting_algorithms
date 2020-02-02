bbce60f6882533edbc0f18b469c659e6b12e8123
Juan-Bogota <1213@holbertonschool.com>
2020-02-02 17:26:07 -0500

    [Feature] Bubble Sort1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,9 +18,11 @@ void bubble_sort(int *array, size_t size)
 	size_t a, b;
 	int tmp;
 
+	if (size == 0)
+		return;
 	for (a = 0; a < size - 1; a++)
 	{
-		for (b = 0; b < size - a - 1 ; b++)
+		for (b = 0; b < size - 1 ; b++)
 		{
 			if (array[b] > array[b + 1])
 			{
