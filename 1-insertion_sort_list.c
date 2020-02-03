08707a3b3ecdf8f30a8da34bf5a57ae3e2287f17
Juan-Bogota <1213@holbertonschool.com>
2020-02-03 18:02:14 +0000

    [feature] insertion Sort List1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,9 +14,10 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = *list, *tmp1 = tmp->next;
 	int index = 0, i;
 
-	if (!(*list) || !(list) || !((*list)->next && (*list)->prev))
+	if (*list == NULL || list == NULL)
 		return;
-
+	if ((*list)->next == NULL && (*list)->prev == NULL)
+		return;
 	while (tmp1 != NULL)
 	{
 		if (tmp->n > tmp1->n)
