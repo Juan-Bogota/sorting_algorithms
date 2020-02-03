bfda4800a87673e22f2f0b8a07b67c83bbc88ad8
Juan-Bogota <1213@holbertonschool.com>
2020-02-03 15:41:12 +0000

    [feature] insertion Sort List

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
 /**
- * create_listint - Creates a doubly linked list from an array of integers
+ * insertion_sort_list - is a simple sorting algorithm that builds
+ * the final sorted array (or list) one item at a time.
  *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
+ * @list: List of an array of integers.
  *
- * Return: Pointer to the first element of the created list. NULL on failure
+ * Return: Void Function
  */
 
 void insertion_sort_list(listint_t **list)
@@ -14,19 +14,20 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = *list, *tmp1 = tmp->next;
 	int index = 0, i;
 
-
-	while(tmp1 != NULL)
+	if(*list == NULL || list == NULL || (*list)->next == NULL)
+		return;
+	while (tmp1 != NULL)
 	{
-		if(tmp->n > tmp1->n)
+		if (tmp->n > tmp1->n)
 		{
 			tmp1->prev = tmp->prev;
 			tmp->next = tmp1->next;
 			tmp->prev = tmp1;
 			tmp1->next = tmp;
-			if(tmp->prev == NULL)
+			if (tmp->prev == NULL)
 				tmp->next->prev = tmp;
-			else if(tmp->next == NULL)
-					tmp1->prev->next = tmp1;
+			else if (tmp->next == NULL)
+				tmp1->prev->next = tmp1;
 			else
 			{
 				tmp->next->prev = tmp;
@@ -34,15 +35,15 @@ void insertion_sort_list(listint_t **list)
 			}
 			print_list(*list);
 			tmp = tmp1->prev;
-			while(tmp != NULL)
+			while (tmp != NULL)
 			{
-				if(tmp->n > tmp1->n)
+				if (tmp->n > tmp1->n)
 				{
 					tmp1->prev = tmp->prev;
 					tmp->next = tmp1->next;
 					tmp->prev = tmp1;
 					tmp1->next = tmp;
-					if(tmp1->prev == NULL)
+					if (tmp1->prev == NULL)
 					{
 						tmp->next->prev = tmp;
 						*list = tmp1;
@@ -60,7 +61,7 @@ void insertion_sort_list(listint_t **list)
 		}
 		tmp = *list;
 		index += 1;
-		for(i = 0; index > i; i++)
+		for (i = 0; index > i; i++)
 		{
 			tmp = tmp->next;
 			tmp1 = tmp->next;
